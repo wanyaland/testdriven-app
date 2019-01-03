4234b4b1022e1d2042d3679e3579879edd18ae40
Harold Wanyama <harold.wanyama@andela.com>
2019-01-04 01:42:08 +0300

    Docker config

---
+++ services/users/project/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import os
+
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)  
+
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
