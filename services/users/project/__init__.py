bde12216afd1eb222c454e0cd61eafbb9464ed49
Harold Wanyama <harold.wanyama@andela.com>
2019-01-07 12:59:37 +0300

    fix pep8

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
-db = SQLAlchemy() 
+db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+
 
 def create_app(script_info=None):
 
@@ -15,6 +18,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
